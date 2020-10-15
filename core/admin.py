37711b18aee3c6c26fe4b5ff11ed80ed29f17459
itonik <itonik@users.noreply.github.com>
2020-10-15 23:55:05 +0300

    Admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+import core.models as models
 
 # Register your models here.
+admin.site.register(models.User)
+admin.site.register(models.Question)
+admin.site.register(models.AnswerOption)
+admin.site.register(models.Answer)
+admin.site.register(models.Pool)
