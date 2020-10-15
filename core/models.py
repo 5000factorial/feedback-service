595d63bf3e81a615652a4348313f938ad5d401a5
itonik <itonik@users.noreply.github.com>
2020-10-15 23:54:58 +0300

    Basic models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User as DjangoUser
 
-# Create your models here.
+
+class User(DjangoUser):
+    pass
+
+
+class Question(models.Model):
+    content = models.JSONField(null=False)
+
+
+class AnswerOption(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=False)
+    text = models.JSONField(null=False)
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=False)
+    option = models.ForeignKey(AnswerOption, on_delete=models.CASCADE, null=False)
+
+
+class Pool(models.Model):
+    questions = models.ManyToManyField(Question)
