fe6f5b6c3421f6a37163cccb64a8e7e31617629e
itonik <itonik@users.noreply.github.com>
2020-10-23 10:22:29 +0300

    import django_heroku

--- feedback_service/settings.py
+++ feedback_service/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
