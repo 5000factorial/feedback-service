ba18d300a8ff3decbe7983369cd27da31f7e7f96
itonik <itonik@users.noreply.github.com>
2020-10-24 14:34:16 +0300

    view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from core.models import Pool
+
+
+class PoolView(DetailView):
+    model = Pool
+
+    def get(self, request):
+        pool = self.object
+        # Template generates here
+        pass
+
+    def post(self, request):
+        pass
