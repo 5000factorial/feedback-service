17db77ef205f1879191d023b02b7c2e60b6768ce
itonik <itonik@users.noreply.github.com>
2020-12-07 12:42:44 +0300

    test_teams_tab_view

--- core/tests.py
+++ core/tests.py
@@ -1,15 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.http import HttpResponse
 
 import core.models as models
+import core.decorators as decorators
+
 
 class DecoratorTestCase(TestCase):
     def setUp(self):
-        self.pool = models.Pool.objects.create('test')
-        self.pool_token = models.PoolToken.objects.create()
+        self.request_factory = RequestFactory()
+        self.pool = models.Pool.objects.create(name='test')
+        self.pool_token = models.PoolToken.objects.create(pool=self.pool)
         self.teams_token = models.TeamsToken.objects.create()
+        
+        def test_view(*args, **kwargs):
+            return HttpResponse()
+
+        self.view = test_view
 
     def test_teams_tab_view(self):
-        pass
+        decorated_view = decorators.teams_tab_view(self.view)
+        response = decorated_view(self.request_factory.get('/'))
+        self.assertEqual(decorators.MS_TEAMS_CONTENT_SECURITY_POLICY,
+                         response['Content-Security-Policy'])
+        self.assertEqual(decorators.MS_TEAMS_CONTENT_SECURITY_POLICY,
+                         response['X-Content-Security-Policy'])            
 
     def test_validate_query_token(self):
         pass
